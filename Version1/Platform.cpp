3b596b3d726661d98f35affa160f0917fedea721
Vozzoi01 <100511770+Vozzoi01@users.noreply.github.com>
2022-05-16 23:52:58 +0930

    Implements enemies, score and comments

--- Version1/Platform.cpp
+++ Version1/Platform.cpp
@@ -1,23 +1,23 @@
 #include "Platform.h"
 #include <iostream>
-
+//constructor which sets platform positon
 Platform::Platform(sf::Color colour, sf::Vector2f size, sf::Vector2f position){
     body = new sf::RectangleShape;
     body->setSize(size);
     body->setPosition(position);
     body->setOrigin(size.x/2,size.y/2);
-    std::cout<<body->getOrigin().y<<std::endl;
     body->setFillColor(colour);
 }
+//draw platform
 void Platform::drawplatform(sf::RenderWindow* win){
     win->draw(*body);
 }
-
+//A collision detecor for platform
 Collision* Platform::getcollider() {
     Collision* bodycolision = new Collision(this->body);
     return bodycolision;
-    }
-
+}
+//destructor
 Platform::~Platform(){
     delete body;
 };
