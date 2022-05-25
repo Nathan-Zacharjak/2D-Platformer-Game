6d46c2b84fac7e140b3bd24f8fc4982531e4a5b9
nathan-zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-05-25 13:57:20 +0930

    Leaderboard now only displays top 3 scores

--- Leaderboard.cpp
+++ Leaderboard.cpp
@@ -5,32 +5,82 @@
 #include "Leaderboard.h"
 
 Leaderboard::Leaderboard(){
+    scoreLimit = 3;
+    lineNumber = 0;
+
     file.open("leaderboard.txt",std::ios::in);
     if(file.is_open()){
         while (std::getline(file,line)){
-            scores.push_back(line);
+            lineNumber++;
+            if (lineNumber % 2 == 0){
+                scoreNumbers.push_back(std::stoi(line));
+            } else {
+                scoreNames.push_back(line);
+            }
         }
     file.close();
     }
+
+    // Sort both the score and name vectors by the highest scores
+    tempScore = -1;
+    tempName = "";
+    for (int i = 0; i < scoreNumbers.size(); i++){
+        for (int j = i; j < scoreNumbers.size(); j++){
+            if (scoreNumbers[i] < scoreNumbers[j]){
+                tempScore = scoreNumbers[i];
+                scoreNumbers[i] = scoreNumbers[j];
+                scoreNumbers[j] = tempScore;
+
+                tempName = scoreNames[i];
+                scoreNames[i] = scoreNames[j];
+                scoreNames[j] = tempName;
+            }
+        }
+    }
+    
 };
 
 std::string Leaderboard::get_scores(){
     formattedScores = "";
-    for (int i = 0; i < scores.size(); i++){
-        formattedScores = formattedScores + scores[i] + "\n";
+    for (int i = 0; i < scoreNumbers.size(); i++){
+        formattedScores = formattedScores + scoreNames[i] + ": " + std::to_string(scoreNumbers[i]) + "\n";
     }
     
     return formattedScores;
 }
 
 void Leaderboard::write_score(std::string Player, int Score){
-    scores.push_back(Player + ": " + std::to_string(Score));
+    scoreNames.push_back(Player);
+    scoreNumbers.push_back(Score);
+
+    // Sort both the score and name vectors by the highest scores
+    tempScore = -1;
+    tempName = "";
+    for (int i = 0; i < scoreNumbers.size(); i++){
+        for (int j = i; j < scoreNumbers.size(); j++){
+            if (scoreNumbers[i] < scoreNumbers[j]){
+                tempScore = scoreNumbers[i];
+                scoreNumbers[i] = scoreNumbers[j];
+                scoreNumbers[j] = tempScore;
+
+                tempName = scoreNames[i];
+                scoreNames[i] = scoreNames[j];
+                scoreNames[j] = tempName;
+            }
+        }
+    }
+
+    // Remove the lowest score from the scoreboard when the score limit is reached
+    if (scoreNumbers.size() > scoreLimit){
+        scoreNumbers.pop_back();
+        scoreNames.pop_back();
+    }
 }
 
 Leaderboard::~Leaderboard(){
     file.open("leaderboard.txt",std::ios::out);
-    for (int i = 0; i < scores.size(); i++){
-        file << scores[i] << "\n";
+    for (int i = 0; i < scoreNumbers.size(); i++){
+        file << scoreNames[i] << "\n" << scoreNumbers[i] << "\n";
     }
     file.close();
 }
--- Leaderboard.h
+++ Leaderboard.h
@@ -11,9 +11,14 @@ using namespace sf;
 class Leaderboard{
     private:
         std::fstream file;
-        std::vector<std::string> scores;
+        std::vector<std::string> scoreNames;
+        std::vector<int> scoreNumbers;
         std::string line;//The temp line of text being read from leaderboard.txt, used in reading the file
         std::string formattedScores;
+        int scoreLimit;//The max number of high scores stored
+        int tempScore;
+        std::string tempName;
+        int lineNumber;
     public:
         Leaderboard();//Reads the leaderboard.txt file and adds any existing scores to the leaderboard
         std::string get_scores();//Returns all scores in the leaderboard as a formatted string for display
